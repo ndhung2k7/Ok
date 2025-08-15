4bbda92bf0a9b280725e03a9737f813705eadde2
ndhung2k7 <hungnguyenveo3@gmail.com>
2025-08-15 18:27:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+  const quotes = [
+            "Tiền tài - Danh vọng - Địa vị - Tình duyên",
+            "Hãy là phiên bản tốt nhất của chính mình!",
+            "Chỉ một lần sống, hãy sống hết mình.",
+            "Đừng chờ đợi, hãy tạo ra cơ hội.",
+            "Đam mê là ngọn lửa dẫn lối thành công."
+        ];
+
+        let quoteIndex = 0;
+        const quoteElement = document.getElementById('quote');
+
+        function changeQuote() {
+            quoteElement.style.opacity = 0;
+            setTimeout(() => {
+                quoteElement.textContent = quotes[quoteIndex];
+                quoteIndex = (quoteIndex + 1) % quotes.length;
+                quoteElement.style.opacity = 1;
+            }, 500);
+        }
+
+        changeQuote();
+        setInterval(changeQuote, 5000);
+document.addEventListener("DOMContentLoaded", function() {
+    const icons = [
+        "https://i.imgur.com/lsWoLZx.gif",
+        "https://i.imgur.com/lhn1OtM.gif",
+        "https://i.imgur.com/xA4gZBm.gif",
+        "fire.gif"
+    ];
+
+    // Chọn ngẫu nhiên 1 icon
+    const randomIcon = icons[Math.floor(Math.random() * icons.length)];
+
+    // Gắn vào ảnh
+    document.getElementById("verifiedIcon").src = randomIcon;
+});
